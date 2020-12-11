4a3159504a2eb7a1507a34d48f440ed8a04d2619
maxkoeppel <m_koeppel@web.de>
2020-12-11 09:08:20 +0100

    major code update

--- helper_functions.py
+++ helper_functions.py
@@ -1,4 +1,3 @@
-
 import random
 import numpy as np
 from keras.models import Sequential
@@ -6,8 +5,6 @@ from keras.layers import InputLayer, Dense, Activation, Dropout
 from keras.layers import Conv2D, MaxPooling2D, Flatten, ZeroPadding2D
 from keras.datasets import cifar10
 from keras.utils import to_categorical
-
-
 
 
 def prepare_data(dataset):
@@ -28,6 +25,60 @@ def prepare_data(dataset):
     return (train_images, train_labels), (test_images, test_labels)
 
 
+def create_model(network):
+    """
+    generates the actual model
+
+    Params:
+      input: dictionary of parameters
+      output: model containing a Pooling and a flattening layer + output for no_classes
+    """
+
+    parameters = network._params
+
+    model = Sequential()
+    model.add(InputLayer(input_shape=INPUT_SHAPE))
+
+    for layer in parameters["layers_2D"]:
+        model.add(
+            Conv2D(
+                filters=parameters["layers_2D"].get(layer).get("units"),
+                kernel_size=parameters["layers_2D"].get(layer).get("kernel"),
+                strides=parameters["layers_2D"].get(layer).get("stride"),
+                padding=parameters["layers_2D"].get(layer).get("padding"),
+                activation=parameters["layers_2D"].get(layer).get("activation"),
+            )
+        )
+
+        model.add(Dropout(parameters["layers_2D"].get(layer).get("dropout")))
+
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Flatten())
+
+    for layer in parameters["layers_dense"]:
+        model.add(
+            Dense(
+                units=parameters["layers_dense"].get(layer).get("units"),
+                activation=parameters["layers_dense"].get(layer).get("activation"),
+            )
+        )
+        model.add(Dropout(parameters["layers_dense"].get(layer).get("dropout")))
+
+    model.add(Dense(NUM_CLASSES, activation=OUTPUT_ACTIVATION))
+    model.compile(
+        loss=LOSS, optimizer=parameters.get("optimizer"), metrics=["accuracy"]
+    )
+    model.fit(
+        train_images,
+        train_labels,
+        batch_size=BATCH_SIZE,
+        epochs=parameters.get("epochs"),
+        verbose=0,
+    )
+
+    return model
+
+
 def assess_networks(networks):
     """
     attempts to build models from each network and
@@ -38,18 +89,19 @@ def assess_networks(networks):
             model = create_model(network)
             accuracy = model.evaluate(test_images, test_labels)[1]
             network._accuracy = accuracy
-            print('Accuracy: {}'.format(network._accuracy))
+            print("Accuracy: {}".format(network._accuracy))
 
         except:
             network._accuracy = 0
-            print ('Build failed.')
+            print("Build failed.")
     return networks
+
 
 def select_best_networks(networks):
     """
     sorts models by best performance and keeps only top 20%
     """
     networks = sorted(networks, key=lambda network: network._accuracy, reverse=True)
-    networks = networks[:int(0.2 * len(networks))]
+    networks = networks[: int(0.2 * len(networks))]
 
     return networks
