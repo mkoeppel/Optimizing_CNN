4f9212954e76e6ce93518c29a4d1dcb0a5579609
maxkoeppel <m_koeppel@web.de>
2020-12-11 09:31:49 +0100

    major code update

--- test_functions.py
+++ test_functions.py
@@ -7,25 +7,28 @@ import random
 import copy
 import numpy as np
 
-from network_improvement import Network, init_networks, rearrange_networks, mutate_network
+from network_improvement import Network, rearrange_networks, mutate_network
 
 def test_Network_class():
     network = Network()
-    parameters = network.get_parameters()
+    parameters = network._params
     assert isinstance(parameters, dict)
 
 def test_parameter_datatype():
     network = Network()
-    parameters = network.get_parameters()
-    assert isinstance(parameters['layers_2D'], list)
+    parameters = network._params
+    assert isinstance(parameters['layers_2D'], dict)
     assert isinstance(parameters['optimizer'], str)
 
 def test_rearrangement():
-    population = 4
-    parents = init_networks(2)
+    POPULATION = 4
+    parents = []
+    networks = []
+    for i in range(POPULATION):
+        parents.append(Network())
     networks = rearrange_networks(parents)
-    offspring_parameters = networks[2].get_parameters()
-    parents_parameters = parents[0].get_parameters()
+    offspring_parameters = networks[2]._params
+    parents_parameters = parents[0]._params
     assert parents_parameters.items() != offspring_parameters.items()
 
 
